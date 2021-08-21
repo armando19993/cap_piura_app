4edc4e9d9de2bd555602fc370d57ab61a37942fd
Jesuar David López <jesuar.lopez@farsiman.com>
2021-08-21 04:26:08 -0600

    Registro de errores.

---
+++ src/app/pasarela/models/causaError.ts
@@ -0,0 +1,4 @@
+export class CausaError{
+    code: string;
+    description: string;
+}
--- src/app/pasarela/pasarela.page.ts
+++ src/app/pasarela/pasarela.page.ts
@@ -1,3 +1,4 @@
+import { CausaError } from './models/causaError';
 import { ServiciosService } from 'src/app/servicios.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,8 +12,6 @@ declare var Mercadopago: any;
   styleUrls: ['./pasarela.page.scss'],
 })
 
-
-
 export class PasarelaPage implements OnInit {
 
  sdkResponseHandler: any[];
@@ -22,6 +21,7 @@ export class PasarelaPage implements OnInit {
   monto: any;
   creditCard: string;
   cardNumber: any;
+  listaErrores: CausaError[] = [];
 
   constructor(
     private http: HttpClient,
@@ -30,11 +30,11 @@ export class PasarelaPage implements OnInit {
     private servicio: ServiciosService
   ) {}
 
-
-
   ngOnInit(){
-    Mercadopago.setPublishableKey("TEST-46e6db42-df00-4ba4-80c8-ce9b9e7303f4");
+    Mercadopago.setPublishableKey('TEST-46e6db42-df00-4ba4-80c8-ce9b9e7303f4');
     Mercadopago.getIdentificationTypes();
+
+    localStorage.removeItem('errores');
 
     this.activeroute.params.subscribe((data: any)=>{
       this.id = data.id;
@@ -43,9 +43,9 @@ export class PasarelaPage implements OnInit {
   }
 
   setCardTokenAndPay(status, response) {
-    if (status == 200 || status == 201) {
-        let form = (<HTMLInputElement>document.getElementById('paymentForm'));
-        let card = <HTMLInputElement>document.createElement('input');
+    if (status === 200 || status === 201) {
+        const form = (<HTMLInputElement>document.getElementById('paymentForm'));
+        const card = <HTMLInputElement>document.createElement('input');
         card.setAttribute('name', 'token');
         card.setAttribute('id', 'token');
         card.setAttribute('type', 'text');
@@ -53,47 +53,66 @@ export class PasarelaPage implements OnInit {
         card.setAttribute('value', response.id);
         form.appendChild(card);
         console.log(response.id);
+
+        return response;
     } else {
-        console.log(response);
-        if(response.error == "bad_request"){
-          this.obtenerCardNumber();
-        }
+      localStorage.removeItem('errores');
+
+      const listaErrores: CausaError[] = [];
+      response?.cause?.forEach(item => {
+        const cause: CausaError = new CausaError();
+        cause.code = item.code;
+        cause.description = item.description;
+
+        listaErrores.push(cause);
+
+      });
+
+      localStorage.setItem('errores',JSON.stringify(listaErrores));
+      return response;
     }
-  };
+  }
+
+  tieneError(errorCode: string) {
+    const lista = JSON.parse(localStorage.getItem('errores')) as CausaError[];
+
+    if (lista === null) {
+      return false;
+    }
+
+    const tiene = lista?.find(x => x.code === errorCode);
+
+    if (tiene !== undefined) { return true; }
+    else { return false; }
+  }
 
   async setPaymentMethod(status, response) {
-    if (status == 200) {
-        let paymentMethod = response[0];
-        (<HTMLInputElement>document.getElementById("paymentMethodId")).value = paymentMethod.id;
-        //console.log((<HTMLInputElement>document.getElementById("paymentMethodId")).value);
-        console.log(response[0].id);
-
+    if (status === 200) {
+      const paymentMethod = response[0];
+      (<HTMLInputElement>document.getElementById('paymentMethodId')).value = paymentMethod.id;
     } else {
-        alert(`payment method info error: ${response}+ ${status}`);
+      alert(`payment method info error: ${response}+ ${status}`);
     }
   };
 
   async setIssuers(status, response) {
-    if (status == 200) {
-        let issuerSelect = <HTMLInputElement>document.getElementById('issuer');
-        response.forEach( issuer => {
-            let opt = document.createElement('option');
-            opt.text = issuer.name;
-            opt.value = issuer.id;
-            issuerSelect.appendChild(opt);
-        });
+    if (status === 200) {
+      const issuerSelect = <HTMLInputElement>document.getElementById('issuer');
+      response.forEach( issuer => {
+          const opt = document.createElement('option');
+          opt.text = issuer.name;
+          opt.value = issuer.id;
+          issuerSelect.appendChild(opt);
+      });
 
     } else {
-        alert(`issuers method info error: ${response}`);
+      alert(`issuers method info error: ${response}`);
     }
   }
 
   async getbancos() {
-    let paymentMethodId = (<HTMLInputElement>document.getElementById("paymentMethodId")).value;
-    Mercadopago.getIssuers(
-        paymentMethodId,
-        this.setIssuers
-    );
+    const paymentMethodId = (<HTMLInputElement>document.getElementById('paymentMethodId')).value;
+    Mercadopago.getIssuers(paymentMethodId,this.setIssuers);
   }
 
   obtenerImagenTarjeta(){
@@ -101,71 +120,55 @@ export class PasarelaPage implements OnInit {
     let paymentMethod = (<HTMLInputElement>document.getElementById("paymentMethodId"))?.value;
 
     if (paymentMethod !== '') {
-      if(paymentMethod == "master"){
-        paymentMethod = "mastercard";
+      if(paymentMethod === 'master') {
+        paymentMethod = 'mastercard';
       }
       return `../../assets/images/${paymentMethod}.png`;
     }
     else{
       return '';
     }
-
-
-
   }
 
-  async guessPaymentMethod(event) {
-    let cardnumber = (<HTMLInputElement>document.getElementById("cardNumber")).value;
-      console.log(cardnumber);
-        Mercadopago.getPaymentMethod({
-            "bin": cardnumber
-        }, this.setPaymentMethod);
+  async guessPaymentMethod() {
+    const cardnumber = (<HTMLInputElement>document.getElementById("cardNumber")).value;
+    Mercadopago.getPaymentMethod({ bin: cardnumber}, this.setPaymentMethod);
+  };
 
+  getCardToken(){
 
+    const $form = (<HTMLInputElement>document.getElementById('paymentForm'));
+    Mercadopago.createToken($form, this.setCardTokenAndPay);
 
- };
+    return false;
+  };
 
- getCardToken(event){
-  // event.preventDefault();
-       let $form = (<HTMLInputElement>document.getElementById('paymentForm'));
-       Mercadopago.createToken($form, this.setCardTokenAndPay);
-       return false;
- };
+  async pagar(){
+    const form = {
+      "transaction_amount": Number(this.monto),
+      "token": (<HTMLInputElement>document.getElementById("token"))?.value,
+      "description": "Pago Servicio, CAP Piura, atraves de Aplicativo Movil",
+      "installments": 1,
+      "payment_method_id": (<HTMLInputElement>document.getElementById("paymentMethodId"))?.value,
+      "payer": { "email": "test@test.com" }
 
- obtenerCardNumber(){
-   this.cardNumber = true;
- }
+    };
 
- async pagar(){
-  let form = {
-   "transaction_amount": Number(this.monto),
-   "token": (<HTMLInputElement>document.getElementById("token"))?.value,
-   "description": "Pago Servicio, CAP Piura, atraves de Aplicativo Movil",
-   "installments": 1,
-   "payment_method_id": (<HTMLInputElement>document.getElementById("paymentMethodId"))?.value,
-   "payer": {
-     "email": "test@test.com"
-   }
+    this.postDataAPI_MP(form).subscribe((val) =>{
+      var arr = JSON.stringify(val);
+      var parsed = JSON.parse (arr);
+    });
+  }
 
- }
-   this.postDataAPI_MP(form).subscribe((val) =>{
+  postDataAPI_MP(bodys) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Credentials":" true",
+      "Authorization": 'Bearer TEST-7704156678097466-010904-a33b280de26eb4d1e747cf84848e3706-696588363',
+      "Access-Control-Allow-Headers":"Content-Type,Access-Control-Allow-Methods, Access-Control-Allow-Origin",
+    });
 
-     var arr = JSON.stringify(val);
-     var parsed = JSON.parse (arr);
-     let arreglo = [];
-     console.log(parsed);
-   });
- }
-
- postDataAPI_MP(bodys){
-  let headers = new HttpHeaders({
-    'Content-Type': 'application/json; charset=UTF-8',
-   'Access-Control-Allow-Origin': '*',
-   "Access-Control-Allow-Credentials":" true",
-   "Authorization": 'Bearer TEST-7704156678097466-010904-a33b280de26eb4d1e747cf84848e3706-696588363',
-   "Access-Control-Allow-Headers":"Content-Type,Access-Control-Allow-Methods, Access-Control-Allow-Origin",
-  });
-
- return this.http.post(this.serverMP, JSON.stringify(bodys), {headers: headers})
-}
+    return this.http.post(this.serverMP, JSON.stringify(bodys), {headers: headers});
+  }
 }
